94c7234a9acefb9070f2993710bf9da6ee9c61e2
YuliiaYB <bolotnya93@gmail.com>
2023-03-26 11:48:01 +0200

    added the functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -1,44 +1,72 @@
 function updateTime() {
   //Los Ahgeles
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //Vienna
   let viennaElement = document.querySelector("#vienna");
-  let viennaDateElement = viennaElement.querySelector(".date");
-  let viennaTimeElement = viennaElement.querySelector(".time");
-  let viennaTime = moment().tz("Europe/Vienna");
+  if (viennaElement) {
+    let viennaDateElement = viennaElement.querySelector(".date");
+    let viennaTimeElement = viennaElement.querySelector(".time");
+    let viennaTime = moment().tz("Europe/Vienna");
 
-  viennaDateElement.innerHTML = viennaTime.format("MMMM Do YYYY");
-  viennaTimeElement.innerHTML = viennaTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    viennaDateElement.innerHTML = viennaTime.format("MMMM Do YYYY");
+    viennaTimeElement.innerHTML = viennaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //Kyiv
   let kyivElement = document.querySelector("#kyiv");
-  let kyivDateElement = kyivElement.querySelector(".date");
-  let kyivTimeElement = kyivElement.querySelector(".time");
-  let kyivTime = moment().tz("Europe/Kyiv");
+  if (kyivElement) {
+    let kyivDateElement = kyivElement.querySelector(".date");
+    let kyivTimeElement = kyivElement.querySelector(".time");
+    let kyivTime = moment().tz("Europe/Kyiv");
 
-  kyivDateElement.innerHTML = kyivTime.format("MMMM Do YYYY");
-  kyivTimeElement.innerHTML = kyivTime.format("h:mm:ss [<small>]A[</small>]");
+    kyivDateElement.innerHTML = kyivTime.format("MMMM Do YYYY");
+    kyivTimeElement.innerHTML = kyivTime.format("h:mm:ss [<small>]A[</small>]");
+  }
 
   //Rome
   let romeElement = document.querySelector("#rome");
-  let romeDateElement = romeElement.querySelector(".date");
-  let romeTimeElement = romeElement.querySelector(".time");
-  let romeTime = moment().tz("Europe/Rome");
+  if (romeElement) {
+    let romeDateElement = romeElement.querySelector(".date");
+    let romeTimeElement = romeElement.querySelector(".time");
+    let romeTime = moment().tz("Europe/Rome");
 
-  romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY");
-  romeTimeElement.innerHTML = romeTime.format("h:mm:ss [<small>]A[</small>]");
+    romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY");
+    romeTimeElement.innerHTML = romeTime.format("h:mm:ss [<small>]A[</small>]");
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city"">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )}<small>${cityTime.format("A")} </small></div>
+        </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
